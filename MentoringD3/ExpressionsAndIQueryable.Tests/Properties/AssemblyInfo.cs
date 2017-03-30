891749725879a77bdb79c5235dec8dde6e146631
Andrei Razumau <Andrei.Razumau@gmail.com>
2017-03-30 19:00:24 +0300

    Initial commit.

---
+++ MentoringD3/ExpressionsAndIQueryable.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExpressionsAndIQueryable.Tests")]
+[assembly: AssemblyDescription("Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrei Razumau")]
+[assembly: AssemblyProduct("ExpressionsAndIQueryable.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d490fc7-f956-4e16-9a32-2b8e73f946c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MentoringD3/ExpressionsAndIQueryable.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionsAndIQueryable.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
