7220f002aa8faa692d95a5fa9728a8369c3717c2
meatGUY <karasik.oleg.nikolaevich@gmail.com>
2017-04-11 09:49:52 +0300

    Comments.

--- MentoringD3/AdvancedXML.Tests/src/SchemaValidation/Tests.cs
+++ MentoringD3/AdvancedXML.Tests/src/SchemaValidation/Tests.cs
@@ -23,6 +23,11 @@ namespace AdvancedXML.Tests.SchemaValidation
             var validator = new SchemaValidator();
             var result = validator.Validate("Resources/xml/wrong-books.xml");
             Assert.AreEqual(false, result.IsSuccess);
+
+            /*
+                How did you ensure that errors returned by "validator" are the errors you have expected to happen?
+            */
+
             foreach (var message in result.Messages)
             {
                 Console.WriteLine(message);
